07924479a2f63568e54efc5deb4da1b62d51e8e2
Marc-Antoine Lemieux <marc@marcantoinelemieux.com>
2016-11-23 14:59:30 -0500

    reorder col attributes

--- components/TwoColumnLayout.js
+++ components/TwoColumnLayout.js
@@ -8,9 +8,9 @@ export default class extends Component {
         const {section} = this.props;
         return <Grid>
                    <Row>
-                       <Col sm={ 4 }
-                            lg={ 3 }
-                            xsHidden>
+                       <Col xsHidden
+                            sm={ 4 }
+                            lg={ 3 }>
                        <Sidebar section={ section } />
                        </Col>
                        <Col xs={ 12 }
